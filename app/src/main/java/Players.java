a474c878c7fbe31feac9aa1113201507de560169
luisipv7 <luisfernandosantos504@gmail.com>
2019-09-25 13:12:03 -0300

    Criada Classe Players

---
+++ app/src/main/java/Players.java
@@ -0,0 +1,32 @@
+public class Players {
+
+    private int idTeam;
+    private String name;
+    private int numberShirt;
+
+    public int getIdTeam() {
+        return idTeam;
+    }
+
+    public void setIdTeam(int idTeam) {
+        this.idTeam = idTeam;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberShirt() {
+        return numberShirt;
+    }
+
+    public void setNumberShirt(int numberShirt) {
+        this.numberShirt = numberShirt;
+    }
+}
+
+
