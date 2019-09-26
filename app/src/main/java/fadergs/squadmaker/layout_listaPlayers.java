ed699c2705707649f468bf80d0b675b480ed654a
luisipv7 <luisfernandosantos504@gmail.com>
2019-09-26 13:45:41 -0300

    Atualizado 26/09/2019

---
+++ app/src/main/java/fadergs/squadmaker/layout_listaPlayers.java
@@ -0,0 +1,14 @@
+package fadergs.squadmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_listaPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_lista_players);
+    }
+}
